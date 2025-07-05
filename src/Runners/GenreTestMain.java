b4a7c84454294681782b144f84fd978e04f8d8c4
felipesergios <felipe.sergio2011@gmail.com>
2025-07-05 10:49:44 -0300

    created manual tests from genre crud operations

---
+++ src/Runners/GenreTestMain.java
@@ -0,0 +1,73 @@
+package Runners;
+
+import Controllers.GenreController;
+import Models.Genre;
+
+import java.util.Scanner;
+
+public class GenreTestMain {
+    public static void main(String[] args) {
+        GenreController genreController = new GenreController();
+        Scanner scanner = new Scanner(System.in);
+        int option;
+
+        do {
+            System.out.println("\n--- GÊNEROS ---");
+            System.out.println("1. Listar gêneros");
+            System.out.println("2. Adicionar gênero");
+            System.out.println("3. Mostrar gênero por nome");
+            System.out.println("4. Remover gênero");
+            System.out.println("0. Sair");
+            System.out.print("Escolha: ");
+            option = scanner.nextInt();
+            scanner.nextLine(); // limpar buffer
+
+            switch (option) {
+                case 1:
+                    System.out.println("\nGêneros cadastrados:");
+                    for (Genre genre : genreController.index()) {
+                        System.out.println("- " + genre.getName());
+                    }
+                    break;
+
+                case 2:
+                    System.out.print("Nome do gênero: ");
+                    String name = scanner.nextLine();
+                    genreController.store(new Genre(name));
+                    System.out.println("Gênero adicionado!");
+                    break;
+
+                case 3:
+                    System.out.print("Nome do gênero para mostrar: ");
+                    String search = scanner.nextLine();
+                    Genre found = genreController.show(search);
+                    if (found != null) {
+                        System.out.println("Gênero encontrado: " + found.getName());
+                    } else {
+                        System.out.println("Gênero não encontrado.");
+                    }
+                    break;
+
+                case 4:
+                    System.out.print("Nome do gênero para remover: ");
+                    String removeName = scanner.nextLine();
+                    if (genreController.delete(removeName)) {
+                        System.out.println("Gênero removido.");
+                    } else {
+                        System.out.println("Gênero não encontrado.");
+                    }
+                    break;
+
+                case 0:
+                    System.out.println("Encerrando...");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida.");
+            }
+        } while (option != 0);
+
+        scanner.close();
+    }
+}
+
