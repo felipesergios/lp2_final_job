ee5e9716a8b53235f7b107c1182d0e3afe580db4
felipesergios <felipe.sergio2011@gmail.com>
2025-07-11 19:41:33 -0300

    Created exception handling in Music Controller

--- src/Controllers/MusicController.java
+++ src/Controllers/MusicController.java
@@ -4,27 +4,35 @@ import java.util.ArrayList;
 import java.util.List;
 public class MusicController {
     private List<Music> songs = new ArrayList<>();
+
     public List<Music> index(){return songs;}
-    public void store(Music music){songs.add(music);}
+
+    public void store(Music music){
+        for(Music data : songs){
+            if(data.getName().equalsIgnoreCase(music.getName())){
+                throw new IllegalArgumentException("A "+music.getName()+" Já existe na base !");
+            }
+        }
+        songs.add(music);
+    }
+
     public Music show(String name){
         for (Music music : songs){
             if(music.getName().equalsIgnoreCase(name)){
                 return music;
             }
         }
-        return null;
+        throw new IllegalArgumentException("Não foi possível encontrar a musica : "+name+"  na base.");
     }
+
     public boolean update(String name,String newName){
         Music music = show(name);
-        if(music != null){music.setName(newName);return true;}
-        return false;
+        music.setName(newName);
+        return true;
     }
     public boolean delete(String name){
         Music music = show(name);
-        if(music != null){
-            songs.remove(music);
-            return true;
-        }
-        return false;
+        songs.remove(music);
+        return true;
     }
 }
